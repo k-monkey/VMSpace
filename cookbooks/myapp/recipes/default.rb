2b3d1a7c78b8c206c9713b02e236c8f96f91f274
k-monkey <captain.monkey88@gmail.com>
2015-07-09 22:16:05 -0700

    learn new tricks about chef and berkshelf

--- cookbooks/myapp/recipes/default.rb
+++ cookbooks/myapp/recipes/default.rb
@@ -5,4 +5,25 @@
 # Copyright (C) 2015 YOUR_NAME
 #
 # All rights reserved - Do Not Redistribute
-#
+template '/tmp/install_nvm.sh' do
+  source 'install_nvm.sh.erb'
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0744'
+end
+
+template '/tmp/install_nvm_pack.sh' do
+  source 'install_nvm_pack.sh.erb'
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0744'
+end
+
+execute "install nvm, node and node libraries. Will take a while..." do               
+    cwd "/home/vagrant"                                                           
+    user "vagrant"                                                                
+    action :run   
+    environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'})                                                             
+    command "/tmp/install_nvm_pack.sh"                  
+end
+
